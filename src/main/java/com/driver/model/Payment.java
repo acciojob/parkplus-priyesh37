c3dae110e07d2096bf34ca5b6df661ab6ad403bf
priyesh37 <priyeshthakker3100@gmail.com>
2024-02-04 22:29:31 +0530

    added complete solution

---
+++ src/main/java/com/driver/model/Payment.java
@@ -0,0 +1,76 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Payment {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.AUTO)
+        private int id;
+
+        private Boolean paymentCompleted;
+
+        @Enumerated
+        private PaymentMode paymentMode;
+
+        @OneToOne
+        @JoinColumn
+        private Reservation reservation;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Boolean getPaymentCompleted() {
+        return paymentCompleted;
+    }
+
+    public void setPaymentCompleted(Boolean paymentCompleted) {
+        this.paymentCompleted = paymentCompleted;
+    }
+
+    public Reservation getReservation() {
+        return reservation;
+    }
+
+    public void setReservation(Reservation reservation) {
+        this.reservation = reservation;
+    }
+
+    public Payment(int id, Boolean paymentCompleted) {
+        this.id = id;
+        this.paymentCompleted = paymentCompleted;
+    }
+    public Payment(){
+
+    }
+
+    public PaymentMode getPaymentMode() {
+        return paymentMode;
+    }
+
+    public void setPaymentMode(PaymentMode paymentMode) {
+        this.paymentMode = paymentMode;
+    }
+
+    public Payment(int id, Boolean paymentCompleted, Reservation reservation) {
+        this.id = id;
+        this.paymentCompleted = paymentCompleted;
+        this.reservation = reservation;
+    }
+
+    public Payment(int id, Boolean paymentCompleted, PaymentMode paymentMode, Reservation reservation) {
+        this.id = id;
+        this.paymentCompleted = paymentCompleted;
+        this.paymentMode = paymentMode;
+        this.reservation = reservation;
+    }
+
+
+}
--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -1,5 +1,7 @@
 package com.driver.services.impl;
 
+import com.driver.model.ParkingLot;
+import com.driver.model.Spot;
 import com.driver.model.SpotType;
 import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.SpotRepository;
@@ -18,26 +20,84 @@ public class ParkingLotServiceImpl implements ParkingLotService {
     SpotRepository spotRepository1;
     @Override
     public ParkingLot addParkingLot(String name, String address) {
+        ParkingLot parkingLot = new ParkingLot();
+        parkingLot.setName(name);
+        parkingLot.setAddress(address);
 
+        parkingLotRepository1.save(parkingLot);
+        return parkingLot;
     }
 
     @Override
     public Spot addSpot(int parkingLotId, Integer numberOfWheels, Integer pricePerHour) {
+
+            Spot spot = new Spot();
+            spot.setPricePerHour(pricePerHour);
+            if(numberOfWheels==2){
+                spot.setSpotType(SpotType.TWO_WHEELER);
+            }
+            else if(numberOfWheels==4){
+                spot.setSpotType(SpotType.FOUR_WHEELER);
+            }
+            else{
+                spot.setSpotType(SpotType.OTHERS);
+            }
+            ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+            spot.setParkingLot(parkingLot);
+
+            List<Spot> spotList = new ArrayList<>();
+            spotList.add(spot);
+            parkingLot.setSpotList(spotList);
+            parkingLotRepository1.save(parkingLot);
+
+            return spot;
 
     }
 
     @Override
     public void deleteSpot(int spotId) {
 
+        Spot spot = spotRepository1.findById(spotId).get();
+
+        ParkingLot parkingLot = spot.getParkingLot();
+
+        parkingLot.getSpotList().remove(spot);
+
+        spotRepository1.deleteById(spotId);
+
     }
 
     @Override
     public Spot updateSpot(int parkingLotId, int spotId, int pricePerHour) {
 
+        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        if(parkingLot != null){
+            Spot spot = spotRepository1.findById(spotId).get();
+            spot.setPricePerHour(pricePerHour);
+
+            spotRepository1.save(spot);
+            return spot;
+        }
+        else {
+            return null;
+        }
+
+//        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+//
+//        parkingLot
+//        List<Spot> spotList = new ArrayList<>();
+//        spotList.add(spot);
+//        parkingLot.setSpotList(spotList);
+//        parkingLotRepository1.save(parkingLot);
+//
+//        return spot;
     }
 
     @Override
     public void deleteParkingLot(int parkingLotId) {
-
+        //        ParkingLot parkingLot = parkingLotRepository1.findById(parkingLotId).get();
+        parkingLotRepository1.deleteById(parkingLotId);
     }
+
+
 }
